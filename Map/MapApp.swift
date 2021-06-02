859d755e338c4b949d8db138452d5f3a11037ad6
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-06-02 15:22:39 -0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Mati Montenegro on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
